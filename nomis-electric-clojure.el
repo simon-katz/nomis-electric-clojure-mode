ad658f833394282925a1cfddecb9e970726506d8
Simon Katz <sk@nomistech.com>
2025-03-22 14:51:25 +0000

    fx

--- nomis-electric-clojure.el
+++ nomis-electric-clojure.el
@@ -370,11 +370,13 @@ PROPERTY is already in PLIST."
     (when (or description -nomis/ec-debug-overlays?)
       (let* ((messages (list description
                              (when -nomis/ec-debug-overlays?
-                               (format "%s DEBUG: Overlay for: %s"
-                                       *-nomis/ec-default-site*
-                                       (reverse tag))))))
+                               (format "DEBUG: Tag = %s"
+                                       (reverse tag)))
+                             (when -nomis/ec-debug-overlays?
+                               (format "DEBUG: Default site = %s"
+                                       *-nomis/ec-default-site*)))))
         (overlay-put ov 'help-echo (-> (-remove #'null messages)
-                                       (string-join " / ")))))
+                                       (string-join "\n")))))
     (unless nomis/ec-color-initial-whitespace?
       ;; We have multiple overlays in the same place, so we need to
       ;; specify their priority.
